8375c1a20b42567d4e795cf20daa0328574a7546
umbertofaenza <umberto.faenza@gmail.com>
2023-09-08 15:48:08 +0200

    feat_appheader: add array of links to data

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,64 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      topMenuLinks: [
+        {
+          text: "characters",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "comics",
+          href: "#",
+          current: true,
+        },
+        {
+          text: "movies",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "tv",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "games",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "collectibles",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "videos",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "fans",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "news",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "shop",
+          href: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
 
-<template>
-  <h2>Header</h2>
-</template>
+<template></template>
 
 <style scoped></style>
