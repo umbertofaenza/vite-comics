6009dec0c585f87dda4fa4b2d7fcf82f5b640fa3
umbertofaenza <umberto.faenza@gmail.com>
2023-09-08 16:45:32 +0200

    feat_appfooter: add footer lists dinamically

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,204 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      dcComicsList: [
+        {
+          text: "characters",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "comics",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "movies",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "tv",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "games",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "collectibles",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "videos",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "fans",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "news",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "shop",
+          href: "#",
+          current: false,
+        },
+      ],
+
+      shopList: [
+        {
+          text: "Shop DC",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "Shop DC Collectibles",
+          href: "#",
+          current: false,
+        },
+      ],
+
+      dcList: [
+        {
+          text: "characters",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "comics",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "movies",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "tv",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "games",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "collectibles",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "videos",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "fans",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "news",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "shop",
+          href: "#",
+          current: false,
+        },
+      ],
+
+      sitesList: [
+        {
+          text: "Shop DC",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "Shop DC Collectibles",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "Shop DC",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "Shop DC Collectibles",
+          href: "#",
+          current: false,
+        },
+        {
+          text: "Shop DC",
+          href: "#",
+          current: false,
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <template>
-  <h2>Footer</h2>
+  <footer>
+    <div class="container">
+      <!-- list wrappers -->
+      <div class="list-wrappers">
+        <!-- left -->
+        <div class="list-wrapper__left">
+          <!-- dc comics list -->
+          <ul>
+            DC COMICS
+            <li v-for="link in dcComicsList">
+              {{ link.text }}
+            </li>
+          </ul>
+
+          <!-- shop list -->
+          <ul>
+            SHOP
+            <li v-for="link in shopList">
+              {{ link.text }}
+            </li>
+          </ul>
+        </div>
+        <!-- center -->
+        <div class="list-wrapper__center">
+          <!-- dc list -->
+          <ul>
+            DC
+            <li v-for="link in dcList">
+              {{ link.text }}
+            </li>
+          </ul>
+        </div>
+        <!-- right -->
+        <div class="list-wrapper__right">
+          <!-- sites list -->
+          <ul>
+            SITES
+            <li v-for="link in sitesList">
+              {{ link.text }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped></style>
