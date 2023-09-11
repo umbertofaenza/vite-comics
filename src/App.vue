b339d155495287deb7598b9d6db600370ae8871c
umbertofaenza <umberto.faenza@gmail.com>
2023-09-11 16:42:56 +0200

    feat: add "quick links" array and create component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
+import AppQuickLinks from "./components/AppQuickLinks.vue";
 
 export default {
-  components: { AppHeader, AppMain, AppFooter },
+  components: { AppHeader, AppMain, AppFooter, AppQuickLinks },
 };
 </script>
 
 <template>
   <AppHeader />
   <AppMain />
+  <AppQuickLinks />
   <AppFooter />
 </template>
 
---
+++ src/data/quick-links.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    text: "Digital Comics",
+    iconPath: "img/buy-comics-digital-comics.png",
+    href: "#",
+  },
+  {
+    text: "DC Merchandise",
+    iconPath: "img/buy-comics-merchandise.png",
+    href: "#",
+  },
+  {
+    text: "Subscriptions",
+    iconPath: "img/buy-comics-subscriptions.png",
+    href: "#",
+  },
+  {
+    text: "Comic Shop Locator",
+    iconPath: "img/buy-comics-shop-locator.png",
+    href: "#",
+  },
+  {
+    text: "DC Power Visa",
+    iconPath: "img/buy-dc-power-visa.svg",
+    href: "#",
+  },
+];
