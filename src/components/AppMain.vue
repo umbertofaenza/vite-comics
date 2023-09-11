e940eb9121150af3943e57946dec1991dd00eb68
umbertofaenza <umberto.faenza@gmail.com>
2023-09-11 15:41:48 +0200

    feat: add jumbotron and comics section's title

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,23 +2,37 @@
 
 <template>
   <main>
+    <!-- jumbotron -->
+    <div class="jumbotron"></div>
+
     <div class="container">
-      <h2>Content goes here</h2>
+      <!-- comics -->
+      <section class="comics">
+        <h1>Current Series</h1>
+        <div class="comics-wrapper"></div>
+      </section>
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
 main {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 150px;
   background-color: #1c1c1c;
 
-  h2 {
-    color: #fff;
+  .jumbotron {
+    height: 400px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    margin-bottom: -30px;
+  }
+
+  .comics {
+    h1 {
+      display: inline-block;
+      background-color: #0282f9;
+      color: #fff;
+      text-transform: uppercase;
+      padding: 15px 30px;
+    }
   }
 }
 </style>
